c5a79269f3faa29fd36e643e756a211e41e3f061
w0nd3r0us <steven.grewal@gmail.com>
2018-07-11 16:02:31 -0400

    completed warmup ex2

--- warmup.js
+++ warmup.js
@@ -3,3 +3,14 @@ var apples = 14
 console.log(apples)
 
 console.log("I have " + apples + " apples.")
+
+// ex2
+var materialsArray = ['wood', 'metal', 'stone']
+
+var wordsObject = {
+  elephant: "The world's largest land mammal.",
+  school: 'A place of learning.',
+  iceCream: 'A delicious milk-based dessert.',
+}
+
+// ex 3
